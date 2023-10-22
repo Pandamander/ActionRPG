86d423098a92b18dddb5dc700bee55eee3ba33f6
cooperwelch <ccooperwelch@gmail.com>
2023-10-22 01:23:16 -0500

    Working on the intro storm sequence. Made a new tilemap, added clouds, gave them parallax, and made the boat move to the right. The hero is currently parented under the tilemap group Foreground, but that will change once I get the new boat sprite uploaded.

--- Assets/Scripts/FloatUpDown.cs
+++ Assets/Scripts/FloatUpDown.cs
@@ -6,6 +6,7 @@ public class FloatUpDown : MonoBehaviour
 {
     [SerializeField] private float floatHeight = 1f;
     [SerializeField] private float floatSpeed = 1f;
+    public float speed;
 
     private Vector3 startPosition;
 
@@ -17,6 +18,6 @@ public class FloatUpDown : MonoBehaviour
     void Update()
     {
         float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        transform.position = new Vector3(transform.position.x + speed, newY, transform.position.z);
     }
 }
