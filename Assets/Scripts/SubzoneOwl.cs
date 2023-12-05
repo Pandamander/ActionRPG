e3fa00556769bec0bef57e34f41f498e71ab8fd4
edahle <edahle72@gmail.com>
2023-12-05 16:58:00 -0600

    WIP Owl Swoop

--- Assets/Scripts/SubzoneOwl.cs
+++ Assets/Scripts/SubzoneOwl.cs
@@ -5,16 +5,49 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class SubzoneOwl : SubzoneEnemy
 {
+    [SerializeField] private Transform player;
     private Animator _animator;
+    private BoxCollider2D _boxCollider;
+    private Vector2 _target;
+    private Vector2 _direction;
+    private bool _flyTowardPlayer;
+    private bool _hasBegunSwoopUp;
 
     public override void Awake()
     {
         base.Awake();
         _animator = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
-    // Update is called once per frame
-    public override void Update()
+
+    private void FixedUpdate()
     {
-        base.Update();
+        if (_flyTowardPlayer)
+        {
+            rigidBody.MovePosition(rigidBody.position + _direction * moveSpeed * Time.fixedDeltaTime);
+
+            if (!_hasBegunSwoopUp && rigidBody.position.y <= _target.y)
+            {
+                _hasBegunSwoopUp = true;
+                _direction = new Vector2(_direction.y, -_direction.x);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _boxCollider.enabled = false;
+            _animator.SetBool("IsAppearing", true);
+        }
+    }
+
+    public void AppearAnimationComplete()
+    {
+        _animator.SetBool("IsFlying", true);
+        _target = new Vector2(player.position.x, player.position.y - 1f);
+        _direction = (new Vector2(_target.x, _target.y) - rigidBody.position).normalized;
+        _flyTowardPlayer = true;
     }
 }
