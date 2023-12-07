1843c72999c34bad5db85d781e00b293e1c04b8d
edahle <edahle72@gmail.com>
2023-12-07 11:46:23 -0600

    titus wake up dialogue wip

--- Assets/Scripts/IntroWreckedShipDialogue.cs
+++ Assets/Scripts/IntroWreckedShipDialogue.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PixelCrushers.DialogueSystem;
 
 public class IntroWreckedShipDialogue : MonoBehaviour
 {
     public GameObject Player;
+    [SerializeField] private DialogueSystemTrigger dialogueTrigger;
     private Animator animator;
     private PlayerMovement movement;
 
@@ -21,18 +23,31 @@ public class IntroWreckedShipDialogue : MonoBehaviour
             OverworldSubzoneContainer.HasShownWreckedShipIntro = true;
             movement.Stop();
             animator.SetBool("IsCollapsed", true);
-            StartCoroutine(GetUp());
+            StartCoroutine(StartSequence());
         }
     }
 
-    private IEnumerator GetUp()
+    private IEnumerator StartSequence()
+    {
+        yield return StartCoroutine(GetUpToCrochIdle());
+
+        // Now do dialogue
+        dialogueTrigger.OnUse(transform);
+
+        // Temp - need to wire up to dialogue complete
+        GetUp();
+    }
+    private IEnumerator GetUpToCrochIdle()
     {
         yield return new WaitForSeconds(4);
         animator.SetBool("IsCollapsed", false);
         animator.SetBool("IsGettingUp", true);
         yield return new WaitForSeconds(2.0f);
         animator.SetBool("IsCrouchIdling", true);
-        yield return new WaitForSeconds(3f);
+    }
+
+    private void GetUp()
+    {
         animator.SetBool("IsStandingUp", true);
         movement.AllowMovement();
     }
