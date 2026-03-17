8a69119c12c7c082f48d08d1bc99ece83cea9f1f
cooperwelch <cooperwelch@users.noreply.github.com>
2026-03-16 23:57:03 -0400

    A bunch of polish to the owl enemy - arcing attack movement, animation state machine polish, and a bug fix where it didn't fly away in the correct direction if Titus approached from the right.

--- Assets/Scripts/SubzoneOwl.cs
+++ Assets/Scripts/SubzoneOwl.cs
@@ -1,71 +1,129 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class SubzoneOwl : SubzoneEnemy
 {
+    public enum OwlState { Idle, Appearing, Attacking, FlyingAway }
+
     [SerializeField] private Transform player;
     [SerializeField] private BoxCollider2D _boxCollider;
-    private Vector2 _target;
-    private Vector2 _direction;
-    private bool _flyTowardPlayer;
-    private bool _hasBegunSwoopUp;
-    private float _swoopUpTimer = 0f;
-    private float _flyUpTimeToLive = 5f;
 
-    public override void Awake()
-    {
-        base.Awake();
-    }
+    [SerializeField] private float swoopOvershoot = 1.5f;
+    [SerializeField] private float windUpHeight = .2f;
+    [SerializeField] private float windUpDuration = 0.4f;
 
-    public override void Update()
-    {
-        base.Update();
-
-        if (_hasBegunSwoopUp)
-        {
-            _swoopUpTimer += Time.deltaTime;
-            if (_swoopUpTimer >= _flyUpTimeToLive)
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (_isDying) {  return; }
-
-        if (_flyTowardPlayer)
-        {
-            rigidBody.MovePosition(rigidBody.position + _direction * moveSpeed * Time.fixedDeltaTime);
-
-            if (!_hasBegunSwoopUp && rigidBody.position.y <= _target.y)
-            {
-                _hasBegunSwoopUp = true;
-                _direction = new Vector2(_direction.y, -_direction.x);
-            }
-        }         
-    }
+    private OwlState _state = OwlState.Idle;
+    private bool _isWindingUp;
+    private Vector2 _windUpStart;
+    private Vector2 _windUpEnd;
+    private float _windUpTimer;
+    private Vector2 _swoopStart;
+    private Vector2 _swoopControl;
+    private Vector2 _swoopTarget;
+    private float _swoopTimer;
+    private float _swoopDuration;
+    private Vector2 _flyAwayDirection;
+    private float _flyAwayTimer;
+    private const float FLY_AWAY_LIFETIME = 5f;
 
     public void AppearTrigger()
     {
         _boxCollider.enabled = false;
         _animator.SetBool("IsAppearing", true);
+        _state = OwlState.Appearing;
     }
 
     public void AppearAnimationComplete()
     {
-        _animator.SetBool("IsFlying", true);
-        _target = new Vector2(player.position.x, player.position.y - 2f);
-        _direction = (new Vector2(_target.x, _target.y) - rigidBody.position).normalized;
-        if (_direction.x > 0f) { FlipFacingDirection(); }
-        _flyTowardPlayer = true;
+        _animator.SetBool("IsAttacking", true);
+
+        _windUpStart = rigidBody.position;
+        _windUpEnd = _windUpStart + Vector2.up * windUpHeight;
+        _windUpTimer = 0f;
+        _isWindingUp = true;
+
+        if (player.position.x > _windUpStart.x) { FlipFacingDirection(); }
+
+        _state = OwlState.Attacking;
+    }
+
+    private void BeginSwoop()
+    {
+        _swoopStart = rigidBody.position;
+        float approachDir = Mathf.Sign(player.position.x - _swoopStart.x);
+        float targetX = player.position.x + approachDir * swoopOvershoot;
+        _swoopTarget = new Vector2(targetX, player.position.y - 0.5f);
+        _swoopControl = new Vector2(_swoopStart.x, _swoopTarget.y);
+        _swoopDuration = Vector2.Distance(_swoopStart, _swoopTarget) / moveSpeed;
+        _swoopTimer = 0f;
+        _isWindingUp = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isDying) return;
+
+        switch (_state)
+        {
+            case OwlState.Attacking:
+                if (_isWindingUp)
+                {
+                    _windUpTimer += Time.fixedDeltaTime;
+                    float wt = Mathf.Clamp01(_windUpTimer / windUpDuration);
+                    rigidBody.MovePosition(Vector2.Lerp(_windUpStart, _windUpEnd, CubicEaseOut(wt)));
+
+                    if (wt >= 1f)
+                    {
+                        BeginSwoop();
+                    }
+                }
+                else
+                {
+                    _swoopTimer += Time.fixedDeltaTime;
+                    float t = Mathf.Clamp01(_swoopTimer / _swoopDuration);
+                    rigidBody.MovePosition(QuadraticBezier(_swoopStart, _swoopControl, _swoopTarget, t));
+
+                    if (t >= 1f)
+                    {
+                        BeginFlyingAway();
+                    }
+                }
+                break;
+
+            case OwlState.FlyingAway:
+                rigidBody.MovePosition(rigidBody.position + _flyAwayDirection * moveSpeed * Time.fixedDeltaTime);
+                _flyAwayTimer += Time.fixedDeltaTime;
+                if (_flyAwayTimer >= FLY_AWAY_LIFETIME)
+                {
+                    Destroy(gameObject);
+                }
+                break;
+        }
+    }
+
+    private void BeginFlyingAway()
+    {
+        _animator.SetBool("IsFlyingAway", true);
+        float horizontalDir = Mathf.Sign(_swoopTarget.x - _swoopStart.x);
+        _flyAwayDirection = new Vector2(horizontalDir, 1f).normalized;
+        _flyAwayTimer = 0f;
+        _state = OwlState.FlyingAway;
     }
 
     private void FlipFacingDirection()
     {
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+    }
+
+    private static Vector2 QuadraticBezier(Vector2 p0, Vector2 p1, Vector2 p2, float t)
+    {
+        float u = 1f - t;
+        return u * u * p0 + 2f * u * t * p1 + t * t * p2;
+    }
+
+    private static float CubicEaseOut(float t)
+    {
+        float u = 1f - t;
+        return 1f - u * u * u;
     }
 }
